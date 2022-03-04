69b4f089d80254af73492bb3a688340f3f4cf253
imaginaryfriendsco <94145604+imaginaryfriendsco@users.noreply.github.com>
2022-03-03 21:41:05 -0500

    Add files via upload

---
+++ generate-images.js
@@ -0,0 +1,125 @@
+#target illustrator
+
+// The layer(s) that will be consistent across all images
+const immutables = ["body"];
+
+
+// Glasses
+function chooseGlasses() {
+	const index = getRandomIndex(glassesLayer);
+	const element = glassesLayer[index];
+	if(element == "none") {
+		return index;
+	}
+	else {
+		groups[element].hidden = false;
+		return index;
+	}
+}
+
+const glassesLayer = ["3D", "clout", "shuttershades", "none"]
+
+
+
+// Body accessory
+function chooseBodyAccessory() {
+	const index = getRandomIndex(bodyAccessoryLayer);
+	const element = bodyAccessoryLayer[index];
+	if(element == "none") {
+		return index;
+	}
+	else {
+		groups[element].hidden = false;
+		return index;
+	}
+}
+
+// There are a lot of different ways to implement rarity
+// One simple solution is to leverage how the random function works so that in this case,
+// fanny pack is less likely to get chosen
+const bodyAccessoryLayer = ["fanny-pack", "none", "none", "none", "none", "none", "none"]
+
+
+// Background
+function chooseBackground() {
+	const index = getRandomIndex(backgroundLayer);
+	const element = backgroundLayer[index];
+	groups[element].hidden = false;
+	return index;
+}
+
+const backgroundLayer = ["green", "blue"]
+
+// Top layer
+const friendLayer = app.activeDocument.layers.getByName("friend");
+
+const groups = friendLayer.groupItems;
+
+var hashes = [];
+
+hideAll();
+generateImages();
+
+
+function generateImages() {
+	var generatedImage = generateSingleImage();
+	var hash = generatedImage[0]
+	for(var i = 0; i < 100; i++) { // Adjust this number accordingly based on # of images
+		while(contains(hashes, hash)) { // Logic to guarantee uniqueness 
+			generatedImage = generateSingleImage();
+			hash = generatedImage[0]
+		}
+		saveAsPNG(generatedImage[1]);
+		hashes.push(hash)
+	}
+}
+
+function generateSingleImage() {
+	hideAll();
+
+	const glasses = chooseGlasses();
+	const bodyAccessory = chooseBodyAccessory();
+	const background = chooseBackground();
+
+	const imageHash = glasses.toString() + "+" + bodyAccessory.toString() + "+" background.toString()
+
+
+	// Naming convention will be important for generating metadata
+	const imageName = glassesLayer[glasses] + "+" + bodyAccessoryLayer[bodyAccessory] + "+" + backgroundLayer[background]
+
+	return [imageHash, imageName];
+}
+
+function getRandomIndex(array) {
+	return Math.floor(Math.random()*array.length)
+}
+
+function hideAll() {
+	for(var i = 0; i < groups.length; i++) {
+		item = groups[i]
+		if(!contains(immutables, item.name))
+			item.hidden = true;
+	}
+}
+
+function saveAsPNG(name) {
+	var fileName = '/Users/Gary/generated-images/' + name + '.png';
+
+    var pngFile = new File(fileName);
+    try {
+        app.activeDocument.imageCapture(pngFile);
+    } catch (e) {
+    }
+}
+
+// Not sure what version of JS they are using but 
+// it can barely be called JS if I had to write this function myself 
+// (not efficient but it works)
+function contains(array, value) {
+	for(var i = 0; i < array.length; i++) {
+		if(array[i] == value) {
+			return true;
+		}
+	}
+	return false;
+}
---
+++ generate-metadata.py
@@ -0,0 +1,40 @@
+source_images = "/Users/Gary/generated-images/"
+
+destination_images = "/Users/Gary/friends/"
+
+friend_metadata = []
+
+
+count = 0
+
+for f in os.listdir(source):
+	if f.startswith("."):
+		continue
+
+	friend_name = "friend_" + str(count) + ".png"
+	friend = {"image": friend_name}
+
+	filename = os.path.basename(f)
+	filename_without_ext = filename.split(".")[0]
+	file_split = filename_without_ext.split("+")
+
+	# Splitting the filename by '+' to extract metadata
+	# This is why file naming convention is important
+	try:
+		attributes = []
+		attributes.append({"trait_type": "Glasses", "value": file_split[0]})
+		attributes.append({"trait_type": "Body accessory", "value": file_split[1]})
+		attributes.append({"trait_type": "Background", "value": file_split[2]})
+	except Exception as e:
+		print(str(e) + ": " + filename)
+
+	friend["attributes"] = attributes
+	friend_metadata.append(friend)
+
+	shutil.copyfile(source + filename, destination + friend_name)
+	count += 1
+
+friends = {"friends": friend_metadata}
+
+with open("/Users/Gary/friends/metadata.json", "w") as outfile:
+	json.dump(friends, outfile)
